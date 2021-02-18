0fa5f10684d7bb4e0f030984e1f9c2451f47843f
Rick Rieger <rickrieger36251@gmail.com>
2021-02-18 13:34:31 -0500

    The the the the that's all folks

--- main.js
+++ main.js
@@ -2,7 +2,35 @@
  * YOUR CODE HERE! *
  *******************/
 
+function yell(string){
+return string + '!';
+}
+function getFirstCharacter(string){
+  return string[0];
+}
+function getLastCharacter(string){
+  return string[string.length - 1];
+}
+function getOneCharacter(string, num){
+  return string[num];
+}
+function getTwoCharacters(string,num,num2){
+  return string[num]+string[num2];
+}
+function makeCapitalized(string){
+  return string.toUpperCase();
+}
+function yellLouder(string){
+  return string.toUpperCase()+'!!!';
+}
 
+
+
+
+function getInitials(string){
+  let x= string.indexOf(' ') + 1;
+  return string[0]+'.'+ string.charAt(x)+'.';
+}
 
 
 
